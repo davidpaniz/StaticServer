bfdffebaadb82994bb4cefaea4ceea9311799cca
davidpaniz <davidpaniz@gmail.com>
2011-05-26 18:16:26 -0300

    fixing class name

--- lib/static_server/static_config.rb
+++ lib/static_server/static_config.rb
@@ -1,29 +1,27 @@
-module Config
-  class StaticConfig
-    def initialize
-      @redirect = {}
-      @forward = {}
-    end
-  
-    def redirect(opt)
-      validate_params(opt)
-      @redirect.merge! opt 
-    end
-    def forward(opt)
-      validate_params(opt)
-      @forward.merge! opt 
-    end
-  
-    def forwards
-      @forward
-    end
-  
-    def redirects
-      @redirect
-    end
+class StaticConfig
+  def initialize
+    @redirect = {}
+    @forward = {}
+  end
+
+  def redirect(opt)
+    validate_params(opt)
+    @redirect.merge! opt 
+  end
+  def forward(opt)
+    validate_params(opt)
+    @forward.merge! opt 
+  end
+
+  def forwards
+    @forward
+  end
+
+  def redirects
+    @redirect
+  end
   private
-    def validate_params(opt)
-      raise "Invalid param" if opt.size != 1
-    end
+  def validate_params(opt)
+    raise "Invalid param" if opt.size != 1
   end
 end
